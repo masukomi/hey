c1b784d0e021edd3782be6052823e6cda6e7bf88
masukomi <masukomi@masukomi.org>
2017-05-27 14:24:44 -0400

    added build.sh

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "deploy type? [clean_gui|local|gui]: "
+read deploy_type
+
+if [ "$deploy_type" = "clean_gui" ]; then
+	echo "doing clean gui build."
+
+	if [ -d "hey" ]; then
+		  rm -rf hey
+	fi
+	mkdir -p hey
+	chicken-install -deploy -p hey/ fmt
+	chicken-install -deploy -p hey/ loops
+	chicken-install -deploy -p hey/ sql-de-lite
+	chicken-install -deploy -p hey/ srfi-13
+	chicken-install -deploy -p hey/ srfi-1
+	chicken-install -deploy -p hey/ pathname-expand
+	chicken-install -deploy -p hey/ numbers
+	chicken-install -deploy -p hey/ toml
+elif [ "$deploy_type" = "local" ]; then
+	echo "doing local build"
+	csc hey.scm
+fi
+
+if [ "$deploy_type" = "clean_gui" ] || [ "$deploy_type" = "gui" ]; then
+	rm -rf hey.app
+	csc -deploy -gui hey.scm
+	cp listicles.so hey.app/Contents/MacOS/
+	cp default.db hey.app/Contents/MacOS/
+	cp -r hey/* hey.app/Contents/MacOS/
+fi
+
+echo "replace /Appications/hey.app ? [y|n]: "
+read replace_it
+if [ "$replace_it" = "y" ]; then 
+	rm -rf /Applications/hey.app
+	cp -r hey.app /Applications/
+	echo "replaced."
+fi
+
