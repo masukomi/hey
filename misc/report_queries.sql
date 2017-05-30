fd5fd4e0917ebf26f65e6342a6e4c03ff97afca5
masukomi <krhodes@onedoor.com>
2017-05-30 17:17:33 -0400

    added to report_queries.sql

--- misc/report_queries.sql
+++ misc/report_queries.sql
@@ -38,3 +38,27 @@ select  strftime('%H', created_at) hours, count(*) interrupts from events group 
 
 -- events since date
 select * from events WHERE strftime('%Y-%m-%d', created_at) BETWEEN '2017-05-27' AND 'now';
+
+-- count of people by events
+ select e.id, ( select count(*) from events_people  where events_people.event_id = e.id)
+
+ from
+  events e 
+  inner join events_people ep on ep.event_id = e.id
+  inner join people p on ep.person_id = p.id
+	group by 1
+  ;
+
+-- list of events that only had person x
+-- actually the event plus the count
+select e.id, ( select count(*) from events_people  where events_people.event_id = e.id) epc
+from
+events e 
+inner join events_people ep on ep.event_id = e.id
+inner join people p on ep.person_id = p.id
+where epc = 1
+and p.id = 2
+group by 1
+;
+
+
